9d465e8be0d49472eac206bc162d5f39c310d0b9
= <>
2024-12-18 20:24:07 +0300

    Upgrade banner.

--- js/banner.js
+++ js/banner.js
@@ -17,11 +17,18 @@ async function renderBanner() {
 	if (bannerData && bannerData.visible === true) {
 		banner.setAttribute("href", bannerData.link)
 		banner.innerHTML += `<img class="banner__image" src="./img/banner.jpg" alt="" />`
-		banner.innerHTML += `<h3 class="banner__text">${bannerData.text}</h3>`
+		banner.innerHTML += `<h3 style="color: ${bannerData.titleTextColor};${bannerData.sideTitleText}: 0.5rem;max-width: ${bannerData.titleWithTitle}" class="banner__text">${bannerData.text}</h3>`
 		const bannerItems = document.querySelector(".banner__items")
+		bannerItems.style.cssText += `${bannerData.sideText}: 0.5rem;`
+		bannerItems.style.cssText += `${
+			bannerData.sideText === "right"
+				? "text-align: end;"
+				: "text-align: start;"
+		}`
+		document.querySelector(".banner__image").style.cssText += `opacity: ${bannerData.opacityImage}`
 
 		bannerData.projects.forEach(item => {
-			bannerItems.innerHTML += `<div class="banner__item">${item}</div>`
+			bannerItems.innerHTML += `<div style="color: ${bannerData.textColor};" class="banner__item">${item}</div>`
 		})
 	}
 }
